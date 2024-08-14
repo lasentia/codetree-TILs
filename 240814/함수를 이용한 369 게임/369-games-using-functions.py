cda14433f7480121a46f9e56c67de415629799ef
Kim, Donghyun <128382645+lasentia@users.noreply.github.com>
2024-08-14 17:38:17 +0900

    Title: 함수를 이용한 369 게임, Time: 258ms, Memory: 70MB, Status: Passed - CodeTree

---
+++ 240814/함수를 이용한 369 게임/369-games-using-functions.py
@@ -0,0 +1,26 @@
+a, b = list(map(int, input().split()))
+
+def multiple_of_tree(a, b):
+    list_a = []
+    for i in range(a, b+1):
+        if i % 3 == 0:
+            list_a.append(i)
+        
+    return list_a
+
+def count_of_num(a, b):
+    list_b = []
+   
+    for i in range(a, b+1):
+        if ('3') in str(i):
+            list_b.append(i)
+
+        elif ('6') in str(i):
+            list_b.append(i)
+
+        elif ('9') in str(i):
+            list_b.append(i)
+
+    return list_b
+
+print(len(set(count_of_num(a, b) + multiple_of_tree(a, b))))
